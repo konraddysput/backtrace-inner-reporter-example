5634701f7288de6a12d745c3e7369be6e049f757
konraddysput <konrad.dysput@gmail.com>
2021-03-01 14:15:27 +0100

    Initial commit

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using Backtrace.Unity;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private BacktraceClient _backtraceClient;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        string universeName = $"your-universe-name.sp.backtrace.io";
+        string token = "token";
+        _backtraceClient = IntegrationFactory.CreateErrorReporter(universeName, token);
+    }
+
+    public void ThrowException()
+    {
+        // throw an exception
+        GameObject prefabButton = null;
+        GameObject goButton = (GameObject)Instantiate(prefabButton);
+        goButton.transform.localScale = new Vector3(1, 1, 1);
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Backtrace inner reporter example
+
+Backtrace example game with inner reporter example that stores a reference to Backtrace-Unity plugin.
+
+## Goal:
+* Validate if .java files are available in final android apk.
